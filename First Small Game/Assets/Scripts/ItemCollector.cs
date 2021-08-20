63c1c03c201edfab0123c3b36018fa35d3e9e1c4
gabrielh00 <a11914741@univie.ac.at>
2021-08-20 14:32:23 +0200

    Rotating Coins and collecting them

---
+++ First Small Game/Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    int counterCoins = 0;
+    [SerializeField] Text coinsText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+            counterCoins++;
+            coinsText.text = "Coins: " + counterCoins;
+        }
+    }
+}
